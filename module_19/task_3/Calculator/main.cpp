ddc7be3b5b2dedf93520016303672f9f4475e18c
Takigudin <al_kuduki_82@mail.ru>
2021-03-25 15:27:51 +0300

    solving part of the problems in module 19

--- module_19/task_3/Calculator/main.cpp
+++ module_19/task_3/Calculator/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int operation(const string &str);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    string str;
+    cin >> str;
+
+    string num1 = str.substr(0,operation(str));
+    string num2 = str.substr(operation(str)+1, str.length() - operation(str));
+
+    double result = 0.0;
+
+    if(str[operation(str)] == '+'){
+        result = std::stod(num1) + std::stod(num2);
+    }else if(str[operation(str)] == '-'){
+        result = std::stod(num1) - std::stod(num2);
+    }else if(str[operation(str)] == '*'){
+        result = std::stod(num1) * std::stod(num2);
+    }else if(str[operation(str)] == '/'){
+        result = std::stod(num1) / std::stod(num2);
+    }
+
+    cout << result << endl;
     return 0;
+}
+
+int operation(const string &str){
+    int res = -1;
+    for(int i = 0; i < str.length(); ++i){
+        if(str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/'){
+            res = i;
+            break;
+        }
+    }
+    return res;
 }
 
