385fcdf3ef5885e9883b2c8c5ef07eef3c718bda
Takigudin <al_kuduki_82@mail.ru>
2021-03-26 09:39:08 +0300

    modified task 3

--- module_19/task_3/Calculator/main.cpp
+++ module_19/task_3/Calculator/main.cpp
@@ -7,28 +7,30 @@ using std::endl;
 using std::string;
 
 int operation(const string &str);
+bool isExpression(const string &str);
 
 int main()
 {
     string str;
     cin >> str;
 
-    string num1 = str.substr(0,operation(str));
-    string num2 = str.substr(operation(str)+1, str.length() - operation(str));
-
-    double result = 0.0;
-
-    if(str[operation(str)] == '+'){
-        result = std::stod(num1) + std::stod(num2);
-    }else if(str[operation(str)] == '-'){
-        result = std::stod(num1) - std::stod(num2);
-    }else if(str[operation(str)] == '*'){
-        result = std::stod(num1) * std::stod(num2);
-    }else if(str[operation(str)] == '/'){
-        result = std::stod(num1) / std::stod(num2);
+    if(operation(str) != -1 && isExpression(str)){
+        string num1 = str.substr(0,operation(str));
+        string num2 = str.substr(operation(str)+1, str.length() - operation(str));
+        double result = 0.0;
+        if(str[operation(str)] == '+'){
+            result = std::stod(num1) + std::stod(num2);
+        }else if(str[operation(str)] == '-'){
+            result = std::stod(num1) - std::stod(num2);
+        }else if(str[operation(str)] == '*'){
+            result = std::stod(num1) * std::stod(num2);
+        }else if(str[operation(str)] == '/'){
+            result = std::stod(num1) / std::stod(num2);
+        }
+        cout << result << endl;
+    }else{
+        cout << "WARNING: Invalid expression!" << endl;
     }
-
-    cout << result << endl;
     return 0;
 }
 
@@ -43,3 +45,14 @@ int operation(const string &str){
     return res;
 }
 
+bool isExpression(const string &str){
+    bool op;
+    for(size_t i = 0; i < str.length(); ++i){
+        op = (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '.');
+        if(!((str[i] >= '0' && str[i] <= '9') || op)){
+            return false;
+            break;
+        }
+    }
+    return true;
+}
