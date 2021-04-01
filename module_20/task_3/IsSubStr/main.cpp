85e70c49dc44e75a023d0e785cf0addca37f76a3
Takigudin <al_kuduki_82@mail.ru>
2021-04-01 11:25:23 +0300

    module 20 completed

--- module_20/task_3/IsSubStr/main.cpp
+++ module_20/task_3/IsSubStr/main.cpp
@@ -7,28 +7,30 @@ bool subStr(const char*, const char*);
 
 int main()
 {
-    char* str = "Hello world";
-    char* str1 = "wor";
-    char* str2 = "banana";
+    const char* str = "Hello world";
+    const char* str1 = "wor";
+    const char* str2 = "banana";
 
     cout << (subStr(str, str1) ? "true" : "false") << endl;
-    cout << (subStr(str1, str2) ? "true" : "false") << endl;
+    cout << (subStr(str, str2) ? "true" : "false") << endl;
 
     return 0;
 }
 
 bool subStr(const char* str1, const char* str2){
     if(!*str1 || !*str2) return false;
-
-    for(int i = 0;*(str1 + i); ++i){
-        int j = 0;
-        if(str1[i] == str2[j]){
-            for(j = 1;*(str1 + j);++j){
-
+    bool res = false;
+    for(int i = 0;*(str1 + i); ++i){         
+        if(str1[i] == str2[0]){
+            res = true;
+            for(int j = 1;*(str2 + j);++j){
+                if(str2[j] != str1[i + j]){
+                    res = false;
+                    break;
+                }
             }
         }
+        if(res)break;
     }
-
-
+    return res;
 }
-
