bdf43c0cd1ffe6dd5f6a5ee1feb3b8b2598a1fe1
Takigudin <al_kuduki_82@mail.ru>
2021-04-07 16:54:15 +0300

    task 2 completed

--- module_22/task_2/TextViewer/main.cpp
+++ module_22/task_2/TextViewer/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <fstream>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char* fileName;
+    cout << "Enter file name: ";
+    cin >> fileName;
+    std::ifstream words;
+
+    words.open(fileName);
+    if(words.is_open()){
+        while(!words.eof()){
+          string word;
+          words >> word;
+          cout << word << " ";
+
+        }
+        words.close();
+    }else{
+        cout << "File not found!" << endl;
+    }
+
     return 0;
 }
 
