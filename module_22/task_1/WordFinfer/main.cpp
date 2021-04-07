0e20aecc55dd8abcb38e74453dc1cbc89c854150
Takigudin <al_kuduki_82@mail.ru>
2021-04-07 16:38:48 +0300

    task 1 completed

--- module_22/task_1/WordFinfer/main.cpp
+++ module_22/task_1/WordFinfer/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <fstream>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    string word;
+    cout << "Enter your search word: ";
+    cin >> word;
+    std::ifstream words;
+    words.open("../src/words.txt");
+
+    if(words.is_open()){
+        int counter = 0;
+        while(!words.eof()){
+          string tmpWord;
+          words >> tmpWord;
+          //cout << tmpWord << " ";
+          if(word == tmpWord) ++counter;
+        }
+        words.close();
+        cout << endl << "number of coincidences: " << counter << endl;
+    }else{
+        cout << "File not found!" << endl;
+    }
+
     return 0;
 }
-
