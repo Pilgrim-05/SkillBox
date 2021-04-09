2391134102399e57e613deccaaa054a4813c1d09
Takigudin <al_kuduki_82@mail.ru>
2021-04-09 11:28:22 +0300

    task 4 completed

---
+++ module_22/task_4/PNG_Detector/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+bool isPng(const string &path);
+
+int main()
+{
+    cout << "Enter the full file name: ";
+    string fileName;
+    cin >> fileName;
+    cout << (isPng(fileName)? "Yes" : "No") << endl;
+
+    return 0;
+}
+
+bool isPng(const string &path){
+    std::ifstream iFile(path.c_str(), std::ios::binary);
+
+    string fileType = path.substr(path.length()-3);
+    if(fileType != "png" && fileType != "PNG") return false;
+    char headNum;
+    string headChar;
+    if(iFile.is_open()){
+        iFile >> headNum;
+        for(int i = 0; i < 3; ++i){
+            char tmp;
+            iFile >> tmp;
+            headChar +=tmp;
+        }
+        iFile.close();
+    }else{
+        cout << "File not found." << endl;
+    }
+
+    return (headNum == -119 && headChar == "PNG");
+}
