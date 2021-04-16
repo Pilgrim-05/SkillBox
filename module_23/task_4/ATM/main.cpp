c304374175d2c01ddefb1e4b49422c538cfdb924
Takigudin <al_kuduki_82@mail.ru>
2021-04-16 15:55:46 +0300

    execution of module 23

---
+++ module_23/task_4/ATM/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
