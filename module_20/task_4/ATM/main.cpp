752b6ddf921143e73409cf4bdfac3e3116972d1e
Takigudin <al_kuduki_82@mail.ru>
2021-07-29 19:03:56 +0300

    task 4

--- module_20/task_4/ATM/main.cpp
+++ module_20/task_4/ATM/main.cpp
@@ -1,14 +1,117 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 #include <fstream>
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
-using std::string;
 
-int main()
+int totalAmount(const std::vector <int> &vec);
+void fillingATM(std::vector <int> &vec);
+void ArrayToFile(const std::vector <int> &vec);
+bool fileToArray(std::vector <int> &vec);
+void printVec(const std::vector <int> &vec);
+
+int main() {
+  const int SIZE = 5;
+  std::vector <int> denominations(SIZE, 0); //the number of each denomination in ATM (100/500/1000/2000/5000)
+
+  char operation;
+  cout << "Select operation (fill (+) / remove (-)): ";
+  cin >> operation;
+  if(operation == '+')
+  {
+    cout << "------Filling ATM------" << endl;
+    fillingATM(denominations);
+    ArrayToFile(denominations);
+  }else if(operation == '-')
+  {
+    cout << "--------Withdrawing funds from an ATM--------" << endl;
+    if(fileToArray(denominations))
+    {
+      cout << "Enter the amount: ";
+      int amount;
+      cin >> amount;
+    }else{
+      cout << "ATM is empty." << endl;
+    }
+
+  }else
+  {
+    cout << "WARNING: The command is not recognized!" << endl;
+  }
+
+  printVec(denominations);
+}
+
+
+bool fileToArray(std::vector <int> &vec)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  std::ifstream fin("atm.bin", std::ios::binary);
+  if(fin.is_open())
+  {
+    int index = 0;
+    while(!fin.eof()){
+      int tmp;
+      fin >> tmp;
+      vec[index] = tmp;
+      ++index;
+    }
+  fin.close();
+
+  }else{
+    cout << "File not found!" << endl;
+  }
+  return totalAmount(vec);
+}
+
+void ArrayToFile(const std::vector <int> &vec)
+{
+  std::ofstream fout("atm.bin", std::ios::binary);
+  if(fout.is_open())
+  {
+    for(int i = 0; i < vec.size(); ++i){
+      fout << vec[i] << endl;
+    }
+
+  }else{
+    cout << "File not found!" << endl;
+  }
+
+  fout.close();
+}
+
+void fillingATM(std::vector <int> &vec)
+{
+  const int MAX_BILL = 1000; // Maximum number of notes in ATM
+  srand(time(nullptr));
+  int emptCells = 0;
+
+  emptCells = MAX_BILL - totalAmount(vec);
+
+  for(int i = 0; i < emptCells; ++i){
+    int bill;
+    bill = rand() % 5;
+    vec[bill]++;
+  }
+}
+
+int totalAmount(const std::vector <int> &vec)
+{
+  int summ = 0;
+  for(int i = 0; i < vec.size(); ++i){
+    summ +=vec[i];
+  }
+  return summ;
+}
+
+void printVec(const std::vector <int> &vec)
+{
+  for(int i = 0; i < vec.size(); ++i){
+    cout << vec[i] << " ";
+  }
+  cout << endl;
 }
 
