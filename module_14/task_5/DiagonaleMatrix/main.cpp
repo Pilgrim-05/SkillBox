7e4974637e273fa0b355eebfc8cb75311dfdc476
Taki <Al_kuduki_82@mail.ru>
2021-09-19 18:27:54 +0300

    optimization

--- module_14/task_5/DiagonaleMatrix/main.cpp
+++ module_14/task_5/DiagonaleMatrix/main.cpp
@@ -5,17 +5,13 @@ using std::cin;
 using std::endl;
 
 void matrixFill(float arr[][4], int);
-void diagonalMatrixs(float arr[][4], int size);
 void printMatrix(float arr[][4], int size);
 
 int main()
 {
     float matrixM[4][4];
-
     matrixFill(matrixM, 4);
-    diagonalMatrixs(matrixM, 4);
     printMatrix(matrixM, 4);
-
     return 0;
 }
 
@@ -29,21 +25,13 @@ void matrixFill(float arr[][4], int size){
     }
 }
 
-void diagonalMatrixs(float arr[][4], int size){
-    for(int i =0; i < size; i++){
-        for (int j = 0; j < size; j++)
-        {
-            if(i != j)arr[i][j] = 0.0;
-        }
-    }
-}
-
 void printMatrix(float arr[][4], int size){
 
     for(int i =0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
         {
+            arr[i][j] *= (i == j);
             cout << arr[i][j] << " ";
         }
         cout << endl;
