190383b8dde78fbaafbf140f21eb35f6770c92ca
Takigudin <al_kuduki_82@mail.ru>
2021-09-23 16:36:43 +0300

    modiffied task 1 from 23 module

--- module_23/task_1/Week/main.cpp
+++ module_23/task_1/Week/main.cpp
@@ -1,41 +1,14 @@
 #include <iostream>
 
-#define MONDAY 1
-#define TUESDAY 2
-#define WEDNESDAY 3
-#define THURSDAY 4
-#define FRIDAY 5
-#define SATURDAY 6
-#define SUNDAY 7
+#define MACR(s) (((s) == 1) ?  "MONDAY" : (((s) == 2) ?  "TUESDAY" : (((s) == 3) ?  "WEDNESDAY" : (((s) == 4) ?  \
+"THURSDAY" : (((s) == 5) ?  "FRIDAY" : (((s) == 6) ?  "SATURDAY" : (((s) == 7) ?  "SUNDAY" : ("ERROR"))))))))
 
-using namespace std;
+using std::cin;
+using std::cout;
 
 int main() {
   std::cout << "Enter the day of the week: ";
   int day;
   cin >> day;
-  if(day == MONDAY)
-  {
-    cout << "MONDAY" << endl;
-  }else if(day == TUESDAY)
-  {
-    cout << "TUESDAY" << endl;
-  }else if(day == WEDNESDAY)
-  {
-    cout << "WEDNESDAY" << endl;
-  }else if(day == THURSDAY)
-  {
-    cout << "THURSDAY" << endl;
-  }else if(day == FRIDAY)
-  {
-    cout << "FRIDAY" << endl;
-  }else if(day == SATURDAY)
-  {
-    cout << "SATURDAY" << endl;
-  }else if(day == SUNDAY)
-  {
-    cout << "SUNDAY" << endl;
-  }else {
-    cout << "wrong weekday number!" << endl;
-  }
+  cout << MACR(day);  
 }
